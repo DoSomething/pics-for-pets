eeaee0035d1daa456f54270681f7ab9f6cdfb774
Michael Chittenden <mchitten@gmail.com>
2013-05-17 11:55:17 -0400

    Paperclip migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   attr_accessible :adopted, :bottom_text, :creation_time, :flagged, :image, :name, :promoted, :share_count, :shelter, :state, :story, :top_text, :type, :update_time
+
+  validates :name,    :presence => true
+  validates :shelter, :presence => true
+  validates :type,    :presence => true
+  validates :state,   :presence => true,
+                      :length => { :maximum => 2 }
+  validates :shelter, :presence => true
+
+  has_attached_file :image, :styles => { :gallery => '450x450!' }, :default_url => '/images/:style/default.png'
 end
