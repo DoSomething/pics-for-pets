e133b1a65b4df3d92708b85950c001db60318755
Michael Chittenden <mchitten@gmail.com>
2013-05-17 12:50:50 -0400

    Changes /posts to be the root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 CreateAndShare::Application.routes.draw do
   resources :posts
 
-
-  root :to => "home#index"
+  root :to => "posts#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
