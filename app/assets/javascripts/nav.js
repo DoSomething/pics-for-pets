47a2d86b410d0d83c0c58c9d4fccc4ea784fbbce
mmwtsn <mwatson@dosomething.org>
2013-05-17 17:44:30 -0400

    Adds state to nav filter

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,9 +1,40 @@
 // Navigation filter functionality
 $(function() {
 
+  // Populate the state filter
+   var states = [
+                  'ALL',
+                  'AK', 'AS', 'AZ', 'AR', 'CA',
+                  'CO', 'CT', 'DE', 'DC', 'FM',
+                  'FL', 'GA', 'GU', 'HI', 'ID',
+                  'IL', 'IN', 'IA', 'KS', 'KY',
+                  'LA', 'ME', 'MH', 'MD', 'MA',
+                  'MI', 'MN', 'MS', 'MO', 'MT',
+                  'NE', 'NV', 'NH', 'NJ', 'NM',
+                  'NY', 'NC', 'ND', 'MP', 'OH',
+                  'OK', 'OR', 'PW', 'PA', 'PR',
+                  'RI', 'SC', 'SD', 'TN', 'TX',
+                  'UT', 'VT', 'VI', 'VA', 'WA',
+                  'WV', 'WI', 'WY'
+                ];
+
+  for (var i=0; i < states.length; i++) {
+    $('#state_filter').append('<option value="' + states[i] + '">' + states[i] + '</option>');
+  }
+
+  // Get user filter input and set destination
   $('#submit_filter').click(function(){
-    var selection = $('#animal_filter').val();
-    $(this).attr('href','/show/' + selection);
+    var animal = $('#animal_filter').val();
+    var state = $('#state_filter').val();
+    var $this = $(this);
+
+    if( $('#state_filter').val() !== 'ALL' ) {
+      $this.attr('href','/show/' + animal + '-' + state);
+    }
+    else {
+      $this.attr('href','/show/' + animal);
+    }
+
   });
 
 });
