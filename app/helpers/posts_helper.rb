c5781abea7670643ff12fa7fe90f11238c6e1937
Michael Chittenden <mchitten@gmail.com>
2013-05-20 09:53:52 -0400

    First try for image writer.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,82 @@
 module PostsHelper
+  def self.find_break_points(text)
+	  count = position = 0
+	  text.scan(/./).each do |c|
+	    count += 1
+	    position += 1
+	    if count == 35
+	      if c == ' '
+	        text[position] = '||'
+	      else
+	        point = [text.index(' ', position - 1), text.rindex(' ', position - 1)].min
+	        text[point] =  '||'
+	      end
+	      count = 1
+	    end
+	  end
+
+	  text
+  end
+  def self.image_writer(path, the_top_text, the_bottom_text)
+  	path = Rails.root.to_s + path
+  	path = path.gsub(/\?.*/i, '')
+
+top_text = self.find_break_points(the_top_text)
+bottom_text = self.find_break_points(the_bottom_text)
+
+top_joined = top_text.split('||').map(&:upcase)
+top_text = top_joined.join("\n")
+top_height = (top_joined.count * 25) + 10
+
+bottom_joined = bottom_text.split('||').map(&:upcase)
+bottom_text = bottom_joined.join("\n")
+bottom_height = (bottom_joined.count * 25) + 10
+
+system '''
+  convert ''' + path + ''' \
+    -strokewidth 0 \
+    -fill "rgba( 0, 0, 0 , 0.25 )" \
+    -draw "rectangle 0,0 450,''' + top_height.to_s + ''' " \
+  ''' + path + '''
+'''
+system '''
+  convert ''' + path + ''' \
+    -gravity North \
+    -font /Users/mchittenden/picsforpets/create-and-share/DINComp-CondBold.ttf \
+    -pointsize 25 \
+    -size 450x \
+    -interline-spacing -13 \
+    -fill black \
+      -annotate -1-1 "''' + top_text + '''" \
+      -annotate +1-1 "''' + top_text + '''" \
+      -annotate +1+1 "''' + top_text + '''" \
+      -annotate -1+1 "''' + top_text + '''" \
+    -fill white \
+      -annotate +0-0 "''' + top_text + '''" \
+  ''' + path + '''
+'''
+system '''
+  convert ''' + path + ''' \
+    -strokewidth 0 \
+    -fill "rgba( 0, 0, 0 , 0.25 )" \
+    -draw "rectangle 450,450 0,''' + (450 - (bottom_height + 6)).to_s + '''" \
+  ''' + path + '''
+'''
+system '''
+  convert ''' + path + ''' \
+    -gravity South \
+    -font /Users/mchittenden/picsforpets/create-and-share/DINComp-CondBold.ttf \
+    -pointsize 25 \
+    -size 450x \
+    -interline-spacing -13 \
+    -fill black \
+      -annotate -1-1 "''' + bottom_text + '''" \
+      -annotate +1-1 "''' + bottom_text + '''" \
+      -annotate +1+1 "''' + bottom_text + '''" \
+      -annotate -1+1 "''' + bottom_text + '''" \
+    -fill white \
+      -annotate +0-0 "''' + bottom_text + '''" \
+  ''' + path + '''
+'''
+  end
 end
