9b944f22516d9befd2b098ff33e374f96f42c1e6
mmwtsn <mwatson@dosomething.org>
2013-05-24 00:54:02 -0400

    Fully functional gate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,24 @@ class ApplicationController < ActionController::Base
   helper_method :authenticated?, :admin?
 
   def authenticated?
-    false
+    (session[:drupal_user_role] && session[:drupal_user_role].values.include?('authenticated user')) ? true : false
+    #true if session[:drupal_user_role].values.include?('authenticated user')
+  end
+
+  def user
+    unless authenticated?
+      flash[:error] = "you need to log in, kid"
+      redirect_to :login
+      false
+    end
   end
 
   def admin?
     false
+    #true if session[:drupal_user_role].values.include?('administrator')
   end
 
-  def authorize
+  def admin
     unless admin?
       flash[:error] = "error: this page is available to admin only - login below"
       redirect_to :login
