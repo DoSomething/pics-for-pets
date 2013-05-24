7244896632e0aa8b78291c427187e6e7e7c5cba0
mmwtsn <mwatson@dosomething.org>
2013-05-24 00:10:40 -0400

    Creates the protected admin and authenticated methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  helper_method :authenticated?, :admin?
+
+  def authenticated?
+    false
+  end
+
+  def admin?
+    false
+  end
+
+  def authorize
+    unless admin?
+      flash[:error] = "error: this page is available to admin only"
+      redirect_to :login
+      false
+    end
+  end
 end
