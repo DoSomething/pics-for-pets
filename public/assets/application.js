e8b533f5205cace8888655e45643590e8ca8d75e
Michael Chittenden <mchitten@gmail.com>
2013-05-28 16:48:01 -0400

    App css

--- public/assets/application.js
+++ public/assets/application.js
@@ -10012,6 +10012,30 @@ $(function() {
     return false;
   });
 });
+// GATE
+// -----
+$(function() {
+  var $login_form = $('#login-form');
+  var $login_link = $('#login-link');
+
+  var $reg_form = $('#registration-form');
+  var $reg_link = $('#registration-link');
+
+  $login_link.click(function() {
+    $reg_form.hide();
+    $login_form.show();
+    return false;
+  });
+
+  $reg_link.click(function() {
+    $reg_form.show();
+    $login_form.hide();
+    return false;
+  });
+
+  // END
+});
+
 // POSTS
 // -----
 $(function() {
