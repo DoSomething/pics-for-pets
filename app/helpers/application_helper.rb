942c3bbaba74e3f03cfab341865b6edff4042b7e
Michael Chittenden <mchitten@gmail.com>
2013-05-28 13:44:43 -0400

    Fixes submitted? method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,6 @@ module ApplicationHelper
   	posts = Post.find_by_uid(user_id)
   	shares = Share.where(:uid => user_id)
 
-  	(user_id && ((!shares.nil? && shares.count > 0) || (!posts.nil? && posts.count > 0)))
+  	(user_id && (!shares.nil? || !posts.nil?))
   end
 end
