3f95e8d27cc95a612871ed3efd1cda0e0ca59d18
mmwtsn <mwatson@dosomething.org>
2013-05-28 13:44:55 -0400

    Adds dynamic width calculation in JavaScript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,17 @@
 // POSTS
 // -----
 $(function() {
+  // AUTOMATICALLY RESIZE BTN WIDTHS
+  set_width = function(parent, child, width) {
+    $(parent).each(function() {
+      var $this = $(this);
+      var child_width = $this.find(child).width() * width;
+      $this.css('width', child_width);
+    });
+  };
+  set_width('a.btn', 'span', 1.3);
+
+  // SHOW & HIDE DEBUG INFORMATION
   $debug = $('.debug');
   $debug.hide();
 
