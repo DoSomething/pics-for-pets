aa476ea5e416512aa534e59924d306b240289bd2
Michael Chittenden <mchitten@gmail.com>
2013-05-31 12:40:41 -0400

    Cleans up image writer a little more.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -21,65 +21,65 @@ module PostsHelper
   	path = Rails.root.to_s + path
   	path = path.gsub(/\?.*/i, '')
 
-    top_text = self.find_break_points(the_top_text)
-    bottom_text = self.find_break_points(the_bottom_text)
-
-    top_joined = top_text.split('||')#.map(&:upcase)
-    top_text = top_joined.join("\n")
-    top_height = (top_joined.count * 25) + 10
-
-    bottom_joined = bottom_text.split('||')#.map(&:upcase)
-    bottom_text = bottom_joined.join("\n")
-    bottom_height = (bottom_joined.count * 25) + 10
-
     font_path = Rails.root.to_s + '/DINComp-Medium.ttf'
 
-    if !top_text.empty?
-    # Top text box
-    system '''
-      convert ''' + path + ''' \
-        -strokewidth 0 \
-        -fill "rgba( 0, 0, 0 , 0.35 )" \
-        -draw "rectangle 0,0 450,''' + top_height.to_s + ''' " \
-      ''' + path + '''
-    '''
+    if !the_top_text.empty?
+      top_text = self.find_break_points(the_top_text)
+      top_text = 'Adopt me because...||' + top_text
+      top_joined = top_text.split('||')
+      top_text = top_joined.join("\n")
+      top_height = (top_joined.count * 25) + 10
 
-    # Top text
-    system '''
-      convert ''' + path + ''' \
-        -gravity North \
-        -font ''' + font_path + ''' \
-        -pointsize 25 \
-        -size 450x \
-        -interline-spacing -13 \
-        -fill white \
-          -annotate +0-0 "''' + top_text + '''" \
-      ''' + path + '''
-    '''
-    end
+      # Top text box
+      system '''
+        convert ''' + path + ''' \
+          -strokewidth 0 \
+          -fill "rgba( 0, 0, 0 , 0.35 )" \
+          -draw "rectangle 0,0 450,''' + top_height.to_s + ''' " \
+        ''' + path + '''
+      '''
 
-    if !bottom_text.empty?
-    # Bottom text box
-    system '''
-      convert ''' + path + ''' \
-        -strokewidth 0 \
-        -fill "rgba( 0, 0, 0 , 0.35 )" \
-        -draw "rectangle 450,450 0,''' + (450 - (bottom_height + 6)).to_s + '''" \
-      ''' + path + '''
-    '''
+      # Top text
+      system '''
+        convert ''' + path + ''' \
+          -gravity North \
+          -font ''' + font_path + ''' \
+          -pointsize 25 \
+          -size 450x \
+          -interline-spacing -13 \
+          -fill white \
+            -annotate +0-0 "''' + top_text + '''" \
+        ''' + path + '''
+      '''
+      end
 
-    # Bottom text
-    system '''
-      convert ''' + path + ''' \
-        -gravity South \
-        -font ''' + font_path + ''' \
-        -pointsize 25 \
-        -size 450x \
-        -interline-spacing -13 \
-        -fill white \
-          -annotate +0-0 "''' + bottom_text + '''" \
-      ''' + path + '''
-    '''
+      if !the_bottom_text.empty?
+        bottom_text = self.find_break_points(the_bottom_text)
+        bottom_text = 'Adopt me because...||' + bottom_text
+        bottom_joined = bottom_text.split('||')
+        bottom_text = bottom_joined.join("\n")
+        bottom_height = (bottom_joined.count * 25) + 10
+      # Bottom text box
+      system '''
+        convert ''' + path + ''' \
+          -strokewidth 0 \
+          -fill "rgba( 0, 0, 0 , 0.35 )" \
+          -draw "rectangle 450,450 0,''' + (450 - (bottom_height + 6)).to_s + '''" \
+        ''' + path + '''
+      '''
+
+      # Bottom text
+      system '''
+        convert ''' + path + ''' \
+          -gravity South \
+          -font ''' + font_path + ''' \
+          -pointsize 25 \
+          -size 450x \
+          -interline-spacing -13 \
+          -fill white \
+            -annotate +0-0 "''' + bottom_text + '''" \
+        ''' + path + '''
+      '''
     end
   end
 
