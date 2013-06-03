f9bf47719cdf67e231a70c22a766969d87c3da04
Michael Chittenden <mchitten@gmail.com>
2013-06-03 13:31:05 -0400

    'nother bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,12 +20,12 @@ module ApplicationHelper
 
   # Did the user already submit something?
   def already_submitted?
-  	user_id = session[:drupal_user_id]
-    Rails.cache.fetch 'already-submitted-' + user_id do
-  	  posts = Post.where(:uid => user_id)
-  	  shares = Share.where(:uid => user_id)
+    user_id = session[:drupal_user_id]
+    Rails.cache.fetch 'already-submitted-' + user_id.to_s do
+      posts = Post.where(:uid => user_id)
+      shares = Share.where(:uid => user_id)
 
-  	  (user_id && (!shares.nil? && shares.count > 0 || !posts.nil? && posts.count > 0))
+      (user_id && (!shares.nil? && shares.count > 0 || !posts.nil? && posts.count > 0))
     end
   end
 end
