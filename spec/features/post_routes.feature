801d06e51c537924b0aa7b11e381be6fe6a872d3
Michael Chittenden <mchitten@gmail.com>
2013-06-06 16:59:37 -0400

    Removes old test.

--- spec/features/post_routes.feature
+++
@@ -1,6 +0,0 @@
-Feature: Test pages
-  We need to check basic routes
-
-  Scenario: Routes
-    Given we visited :index
-    Then the page should redirect to :login
--- spec/features/step_definitions/basic_steps.rb
+++
@@ -1,7 +0,0 @@
-Given /we visited ([A-z]*)/ do |action|
-  visit(action.to_sym)
-end
-
-Then /the page should redirect to (.*)/ do |greeting|
-  expect(response).to redirect_to greeting.to_sym
-end
