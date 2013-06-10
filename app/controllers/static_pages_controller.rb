ee2a5c57140fb157cffd58cdd0abc209d1f9dcb9
Michael Chittenden <mchitten@gmail.com>
2013-06-10 12:18:41 -0400

    Static pages, users.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
+  # GET /start
   def guide
   end
 
+  # GET /
+  # This is for when the campaign closes -- static HTML for the finished gallery.
   def gallery
   end
 end
