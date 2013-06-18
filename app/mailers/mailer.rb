2dee14d592f1ea54fc09cfcc1b29e7cca211976c
Michael Chittenden <mchitten@gmail.com>
2013-06-18 11:49:45 -0400

    Deliver the email.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,5 +1,5 @@
 class Mailer < ActionMailer::Base
-  default from: "Hilary at DoSomething.org <animals@dosomething.org>"
+  default :from => "Hilary at DoSomething.org <animals@dosomething.org>"
 
   def signup(email)
   	mail(:to => email, :subject => "Thanks for signing up for Pics for Pets!")
