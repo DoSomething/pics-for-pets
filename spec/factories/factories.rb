10877d446ab3a4c1f0d4672c37c727cc838e96c9
Michael Chittenden <mchitten@gmail.com>
2013-06-26 16:57:12 -0400

    Introduces Factory Girl into tests

---
+++ spec/factories/factories.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :api_key do
+    key 'aea12e3fe5f83f0d574fdff0342aba91'
+  end
+
+  factory :post do
+    uid 703718
+    adopted false
+    meme_text 'Bottom text'
+    meme_position 'bottom'
+    flagged false
+    image File.new(Rails.root + 'spec/mocks/ruby.png')
+    name 'Spot the kitten'
+    promoted false
+    share_count 0
+    shelter 'Cats'
+    state 'PA'
+    city 'Pittsburgh'
+    story "This is a story"
+    animal_type 'cat'
+  end
+end
