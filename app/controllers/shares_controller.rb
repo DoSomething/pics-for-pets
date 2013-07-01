f5b95e9a8a8df7decc77ab32382edfb41acf2b5a
Michael Chittenden <mchitten@gmail.com>
2013-07-01 13:48:57 -0400

    Wrong condition.

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -10,7 +10,7 @@ class SharesController < ApplicationController
     # Note: we can't put this in a model.  Models can't access the session variable.
     @share = Share.new(params[:share])
     @post = Post.find(params[:share][:post_id])
-    if @post.share_count.nil? || @post.share_count != 0
+    if @post.share_count.nil? || @post.share_count == 0
       @post.share_count = params[:new_count]
     else
       @post.share_count = (@post.share_count += 1)
